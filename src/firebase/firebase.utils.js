61d8f734a7a292ec2a8531ed32ae29fd9775b7da
srijita kiki <kikisrijita@gmail.com>
2020-03-19 03:37:07 +0600

    We've moved our shop data into our firestore database, now we need to modify our application in such a way that considers our data being loaded asynchronously

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -17,7 +17,10 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
   if (!userAuth) return;
 
   const userRef = firestore.doc(`users/${userAuth.uid}`);
+  const collectionRef = firestore.collection("users");
   const snapShot = await userRef.get();
+  const collectionSnapshot = collectionRef.get();
+  console.log({ collectionSnapshot });
 
   if (!snapShot.exists) {
     const { displayName, email } = userAuth;
@@ -35,6 +38,37 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
     }
   }
   return userRef;
+};
+
+export const addCollectionAndDocuments = async (
+  collectionKey,
+  objectsToAdd
+) => {
+  const collectionRef = firestore.collection(collectionKey);
+  const batch = firestore.batch();
+  objectsToAdd.forEach(obj => {
+    const newDocRef = collectionRef.doc();
+    batch.set(newDocRef, obj);
+  });
+  return await batch.commit();
+};
+
+export const convertCollectionsSnapshotToMap = collections => {
+  const transformedCollection = collections.docs.map(doc => {
+    const { title, items } = doc.data();
+
+    return {
+      routeName: encodeURI(title.toLowerCase()),
+      id: doc.id,
+      title,
+      items
+    };
+  });
+
+  return transformedCollection.reduce((accumulator, collection) => {
+    accumulator[collection.title.toLowerCase()] = collection;
+    return accumulator;
+  }, {});
 };
 
 firebase.initializeApp(config);
