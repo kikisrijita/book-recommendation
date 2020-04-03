3bc0561931d35d7b5fefdfe0de9fd5424cf3869b
srijita kiki <kikisrijita@gmail.com>
2020-04-04 01:05:09 +0600

    saga final commit

--- src/components/sign-up/sign-up.component.jsx
+++ src/components/sign-up/sign-up.component.jsx
@@ -3,8 +3,6 @@ import React from "react";
 import FormInput from "../form-input/form-input.component";
 import CustomButton from "../custom-button/custom-button.component";
 
-import { auth, createUserProfileDocument } from "../../firebase/firebase.utils";
-
 import "./sign-up.styles.scss";
 import { connect } from "react-redux";
 import { signUpStart } from "../../redux/user/user.actions";
